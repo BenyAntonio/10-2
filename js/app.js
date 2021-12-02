9d3df1e35fc7c5232f3303ff9e941f9dbbe230c9
BenyAntonio <95263831+BenyAntonio@users.noreply.github.com>
2021-12-01 20:18:21 -0600

    Delete app.js

--- js/app.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('../sw.js').then(() => {
-			console.log('soy el servceWorker registrado y funcionando.')
-		})
-	})
-}
